e03ab5fded91b91ac265b1ad33e62465051648df
coding4freedom <chawkins2010@hotmail.com>
2024-12-26 23:54:29 -0600

    wild hog image added assets and home page dynamic loading

--- src/components/SpeciesTileComponent.jsx
+++ src/components/SpeciesTileComponent.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import SpeciesComponent from './SpeciesComponent';
 import python from '../assets/python.png';
+import hog from '../assets/hog.jpg';
 import fox from '../assets/fox.jpg';
 import './SpeciesTileComponent.css';
 
 const SpeciesTileComponent = () => {
     const species = [
         {id: 0, path: '/pythons', title: 'Burmese Pythons', src: `${python}`, name: 'python'},
-        {id: 1, path: '/wild_hogs', title: 'Wild Hogs', src: `${fox}`, name: 'hogs'},
+        {id: 1, path: '/wild_hogs', title: 'Wild Hogs', src: `${hog}`, name: 'hogs'},
         {id: 2, path: '/carps', title: 'Asian Carp', src: `${fox}`, name: 'carps'},
     ];
 
